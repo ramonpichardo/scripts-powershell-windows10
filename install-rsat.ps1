7695d8d16a8c71296af6d74779d6f440fe7bbe3d
Ramon <ramonpichardo+github@gmail.com>
2019-06-22 00:33:00 -0700

    Mitigate Add-WindowsCapability error 0x800f0954

--- install-rsat.ps1
+++ install-rsat.ps1
@@ -3,6 +3,14 @@
 # Install Windows Optional Feature = Remote Server Administration Tools (RSAT)
 # Administrator PowerShell console = Yes
 
+# Mitigation for Add-WindowsCapability failure with error code 0x800f0954 :
+# Run "gpedit.msc" (Local Group Policy Editor) to edit your local computer policy
+# Go to "Computer Configuration\Administrative Templates\System\Specify settings for optional component installation and component repair"
+# Double-click the above policy or click "Edit policy setting"
+# Set the policy to "Enabled"
+# Enable the option by selecting the checkbox labeled "Download repair content and optional features directly from Windows Update instead of Windows Server Update Services (WSUS)"
+# Click OK to save and close the policy
+
 Set-ExecutionPolicy RemoteSigned
 
 # View descriptions of all RSAT items
