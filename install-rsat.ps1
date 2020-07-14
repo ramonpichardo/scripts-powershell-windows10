cb1e3907fd6253807fe050d438e0f257dbd8a280
Ramon <ramonpichardo+github@gmail.com>
2020-07-14 16:40:48 -0700

    Update installation instructions

--- install-rsat.ps1
+++ install-rsat.ps1
@@ -1,29 +1,29 @@
 <# 
-Prerequisites:
+Prerequisites 1 of 2: System
 - OS = Windows 10 Enterprise, build version 1809
-- Install Windows Optional Feature = Remote Server Administration Tools (RSAT)
 - Administrator PowerShell console = Yes
 
-Mitigation for Add-WindowsCapability failure with error code 0x800f0954 :
+Prerequisites 2 of 2: Local Policy Change (Mitigation for Add-WindowsCapability failure with error code 0x800f0954)
 1. Run Local Group Policy Editor ("gpedit.msc") as an Administrator to edit your local computer policy.
 2. Go to "Computer Configuration\Administrative Templates\System\".
 3. While the subfolder "System" is selected, find the following policy in the right pane: "Specify settings for optional component installation and component repair".
 4. Double-click the above stated policy or click "Edit policy setting".
 5. Set the policy to "Enabled".
-6. Enable the option by selecting the checkbox labeled "Download repair content and optional features directly from Windows Update instead of Windows Server Update Services (WSUS)".
+6. Select the checkbox of the policy's option labeled "Download repair content and optional features directly from Windows Update instead of Windows Server Update Services (WSUS)".
 7. Click OK to save and close the policy.
 #>
 
+# Update the system's local execution policy
 Set-ExecutionPolicy RemoteSigned
 
-# View descriptions of all RSAT items
+# Optional: View descriptions of all RSAT items
 # Get-WindowsCapability -Name RSAT* -Online
 
-# View current installation state of all RSAT items 
+# Optional: View current installation state of all RSAT items 
 # Get-WindowsCapability -Name RSAT* -Online | Select-Object -Property DisplayName, State
 
 # Install all RSAT items
 Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
 
 # Optional: Update help content to include RSAT items
-Update-Help
+# Update-Help
