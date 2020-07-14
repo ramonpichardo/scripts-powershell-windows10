ac3e2a66151be5f85e2c187c090469ade252594b
Ramon <ramonpichardo+github@gmail.com>
2020-07-14 15:49:37 -0700

    Update installation instructions

--- install-rsat.ps1
+++ install-rsat.ps1
@@ -1,15 +1,18 @@
-# Prerequisites:
-# OS = Windows 10 Enterprise version 1809
-# Install Windows Optional Feature = Remote Server Administration Tools (RSAT)
-# Administrator PowerShell console = Yes
+<# 
+Prerequisites:
+OS = Windows 10 Enterprise version 1809
+Install Windows Optional Feature = Remote Server Administration Tools (RSAT)
+Administrator PowerShell console = Yes
 
-# Mitigation for Add-WindowsCapability failure with error code 0x800f0954 :
-# Run "gpedit.msc" (Local Group Policy Editor) to edit your local computer policy
-# Go to "Computer Configuration\Administrative Templates\System\Specify settings for optional component installation and component repair"
-# Double-click the above policy or click "Edit policy setting"
-# Set the policy to "Enabled"
-# Enable the option by selecting the checkbox labeled "Download repair content and optional features directly from Windows Update instead of Windows Server Update Services (WSUS)"
-# Click OK to save and close the policy
+Mitigation for Add-WindowsCapability failure with error code 0x800f0954 :
+1. Run "gpedit.msc" (Local Group Policy Editor) to edit your local computer policy.
+2. Go to "Computer Configuration\Administrative Templates\System\
+3. While the subfolder "System" is selected, find the following policy in the right pane: "Specify settings for optional component installation and component repair".
+4. Double-click the above stated policy or click "Edit policy setting".
+5. Set the policy to "Enabled".
+6. Enable the option by selecting the checkbox labeled "Download repair content and optional features directly from Windows Update instead of Windows Server Update Services (WSUS)".
+7. Click OK to save and close the policy.
+#>
 
 Set-ExecutionPolicy RemoteSigned
 
@@ -22,5 +25,5 @@ Set-ExecutionPolicy RemoteSigned
 # Install all RSAT items
 Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
 
-# Update help content to include RSAT items
+# Optional: Update help content to include RSAT items
 Update-Help
